216373619804b74c95521db21f6ec91da1f4b4f2
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-02-24 14:18:27 -0600

    Add files via upload

---
+++ app_api/routes/Assign4.routes.js
@@ -0,0 +1,21 @@
+module.exports = function(app) {
+
+    var Assign4 = require('./routes/list.js');
+
+
+
+    // Create a new Note
+    app.post('/Assign4s', Assign4s.create);
+
+    // Retrieve all Notes
+    app.get('/Assign4s', Assign4s.findAll);
+
+    // Retrieve a single Note with noteId
+    app.get('/Assign4s/:Assign4Id', Assign4s.findOne);
+
+    // Update a Note with noteId
+    app.put('/Assign4s/:Assign4Id', Assign4s.update);
+
+    // Delete a Note with noteId
+    app.delete('/Assign4s/:Assign4Id', Assign4s.delete);
+}

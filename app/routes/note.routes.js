0a4955fa46a670aff7e62aae33307f3a1f897c81
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-02-24 16:23:52 -0600

    Add files via upload

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,1 +1,19 @@
+module.exports = function(app) {
 
+    var notes = require('../controllers/note.controller.js');
+
+    // Create a new Note
+    app.post('/users/:id/notes/', notes.create);
+
+    // Get all Notes matching Username
+    app.get('/users/:id/notes', notes.findUser);
+
+    // Retrieve a single Note matching Title
+    app.get('/users/:id/notes/:title', notes.findTitle);
+
+    // Update a Note with specified Title
+    app.put('/users/:id/notes/:title', notes.update);
+
+    // Delete a Note with specified Title
+    app.delete('/users/:id/notes/:title ', notes.delete);
+};

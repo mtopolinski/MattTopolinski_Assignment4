79c041f333464c3e3e6d6dc963c3a3b72bea3089
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-02-24 16:23:09 -0600

    Add files via upload

--- app/models/note.model.js
+++ app/models/note.model.js
@@ -1,1 +1,10 @@
+var mongoose = require('mongoose');
 
+var NoteSchema = mongoose.Schema({
+    username: String,
+    //content: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Note', NoteSchema);

e504e57f287ce8db386b7d5044d121b17c097420
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-02-23 20:01:32 -0600

    Add files via upload

--- routes/list.js
+++ routes/list.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/Assign4';
+mongoose.connect(dbURI);
+
 /* GET list. */
 router.get('/', function(req, res, next) {
     res.render('list', { title: 'List'});
+});
+
+router.get('/models/db', function(req, res, next) {
+dbURI.collection('Assign4').find();
 });
 
 module.exports = router;

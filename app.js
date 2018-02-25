b34a6087a4a51ade12a5685f390105831ae8683d
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-02-24 19:36:52 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//require('./models/db');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -43,26 +42,13 @@ require('./app/routes/note.routes.js')(app);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', index);
 app.use('/users', users);
 app.use('/list', list);
 app.use('/listEdit', listEdit);
 app.use('/login', login);
-//app.use('/api', routesApi);
-
-/*
-app.get('/models/db', function(req, res) {
-    var cursor = dbURI.collection('Assign4').find();
-});
-*/
 
 
 // define a simple route

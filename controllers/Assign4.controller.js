f44019da1e1fdd44ce3c7fb5fca34944e125a8d2
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-02-24 19:38:20 -0600

    Add files via upload

--- controllers/Assign4.controller.js
+++ controllers/Assign4.controller.js
@@ -1,44 +1,72 @@
-var Assign4 = require('./models/Assign4.model.js');
-
-exports.create = function(req, res) {
-    // Create and Save a new Note
-
-};
-
-exports.findAll = function(req, res) {
-    // Retrieve and return all notes from the database.
-
-};
-
-exports.findOne = function(req, res) {
-    // Find a single note with a noteId
-
-};
-
-exports.update = function(req, res) {
-    // Update a note identified by the noteId in the request
-
-};
-
-exports.delete = function(req, res) {
-    // Delete a note with the specified noteId in the request
-
-};
 
 exports.create = function(req, res) {
     // Create and Save a new Note
     if(!req.body.content) {
-        res.status(400).send({message: "Assign4 can not be empty"});
+        res.status(400).send({message: "Note can not be empty"});
     }
-    var assign4 = new Assign4({title: req.body.title || "Untitled Entry", content: req.body.content});
 
-    assign4.save(function(err, data) {
-        console.log(data);
+    var note = new Note({title: req.body.title || "Untitled Note", content: req.body.content});
+
+    note.save(function(err, data) {
         if(err) {
             console.log(err);
-            res.status(500).send({message: "Some error occurred while creating the Assign entry."});
+            res.status(500).send({message: "Some error occurred while creating the Note."});
         } else {
             res.send(data);
         }
     });
 };
+
+exports.findAll = function(req, res) {
+    // Retrieve and return all notes from the database.
+    Note.find(function(err, notes){
+        if(err) {
+            res.status(500).send({message: "Some error occurred while retrieving notes."});
+        } else {
+            res.send(notes);
+        }
+    });
+};
+
+exports.findOne = function(req, res) {
+    // Find a single note with a noteId
+    Note.findById(req.params.noteId, function(err, data) {
+        if(err) {
+            res.status(500).send({message: "Could not retrieve note with id " + req.params.noteId});
+        } else {
+            res.send(data);
+        }
+    });
+};
+
+exports.update = function(req, res) {
+    // Update a note identified by the noteId in the request
+    Note.findById(req.params.noteId, function(err, note) {
+        if(err) {
+            res.status(500).send({message: "Could not find a note with id " + req.params.noteId});
+        }
+
+        note.username = req.body.username;
+        //note.content = req.body.content;
+
+        note.save(function(err, data){
+            if(err) {
+                res.status(500).send({message: "Could not update note with id " + req.params.noteId});
+            } else {
+                res.send(data);
+            }
+        });
+    });
+};
+
+exports.delete = function(req, res) {
+    // Delete a note with the specified noteId in the request
+    Note.remove({_id: req.params.noteId}, function(err, data) {
+        if(err) {
+            res.status(500).send({message: "Could not delete note with id " + req.params.id});
+        } else {
+            res.send({message: "Note deleted successfully!"})
+        }
+    });
+};
+

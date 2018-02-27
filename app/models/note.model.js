9161c7652c4deb5eaaf21f9114926138aee3b216
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-02-26 20:36:24 -0600

    Add files via upload

--- app/models/note.model.js
+++ app/models/note.model.js
@@ -1,5 +1,23 @@
 var mongoose = require('mongoose');
 
+var dbURI = 'mongodb://localhost/note';
+mongoose.connect(dbURI);
+
+// Configuring the database
+
+
+mongoose.Promise = global.Promise;
+
+
+mongoose.connection.on('error', function() {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
+mongoose.connection.once('open', function() {
+    console.log("Successfully connected to the database");
+});
+
 var NoteSchema = mongoose.Schema({
     Username: String,
     Password: String,
@@ -7,8 +25,6 @@ var NoteSchema = mongoose.Schema({
         Title: String,
         Contents: String
     }
-}, {
-    timestamps: true
 });
 
 module.exports = mongoose.model('Note', NoteSchema);

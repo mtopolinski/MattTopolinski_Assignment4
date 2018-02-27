75d04520a302c11326af58a6f220d7e8003f05ad
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-02-26 20:36:46 -0600

    Add files via upload

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,19 +1,20 @@
-module.exports = function(app) {
+var express = require('express');
+var router = express.Router();
+var notes = require('../controllers/note.controller.js');
 
-    var notes = require('../controllers/note.controller.js');
+// Create a new Note
+router.post('/users/:id/notes/', notes.create);
 
-    // Create a new Note
-    app.post('/users/:id/notes/', notes.create);
+// Get all Notes matching Username
+router.get('/users/:id/notes', notes.findUser);
 
-    // Get all Notes matching Username
-    app.get('/users/:id/notes', notes.findUser);
+// Retrieve a single Note matching Title
+router.get('/users/:id/notes/:title', notes.findTitle);
 
-    // Retrieve a single Note matching Title
-    app.get('/users/:id/notes/:title', notes.findTitle);
+// Update a Note with specified Title
+router.put('/users/:id/notes/:title', notes.update);
 
-    // Update a Note with specified Title
-    app.put('/users/:id/notes/:title', notes.update);
+// Delete a Note with specified Title
+router.delete('/users/:id/notes/:title ', notes.delete);
 
-    // Delete a Note with specified Title
-    app.delete('/users/:id/notes/:title ', notes.delete);
-};
+module.exports = router;

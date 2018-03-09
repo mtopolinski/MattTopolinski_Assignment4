2fc08b2b5dfbf3a708673ec37e51f9639a868fbf
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-03-08 21:25:43 -0600

    Add files via upload

--- routes/list.js
+++ routes/list.js
@@ -2,14 +2,37 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser'); //parses information from POST
 var methodOverride = require('method-override'); //used to manipulate POST
+var request = require('request');
 
 var mongoose = require('mongoose');
 
 
-
 /* GET list. */
 router.get('/', function(req, res, next) {
-    res.render('list', { title: 'List'});
+
+    //this is what it should be, I hard coded it to make it work as the route is set up now.
+    //const path = 'http://localhost:3000/test/users/' + req.params.id + '/notes';
+
+
+    const path = 'http://localhost:3000/test/users/5a949e313981a7ae24c365cb/notes';
+
+    const requestOptions = {
+        url : path,
+        method : 'GET',
+        json : {}
+    };
+
+    request(
+        requestOptions,
+        (function (err, response, body) {
+
+            console.log(body);
+
+            res.render('list', { title: 'List', notes: body});
+        })
+    );
+
+
 });
 
 

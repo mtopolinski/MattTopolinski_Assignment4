bfc85c4cb63c269eccfd5860c4a84d8d0a3e49b2
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-03-10 22:31:57 -0600

    Add files via upload

--- routes/listAdd.js
+++ routes/listAdd.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser'); //parses information from POST
+var methodOverride = require('method-override'); //used to manipulate POST
+var request = require('request');
 
 /* GET Add page. */
 router.get('/', function(req, res, next) {
@@ -11,9 +14,36 @@ router.post('/', function(req, res, next) {
 
     console.log('listAdd post from form');
     console.log(req.body.title);
-    console.log(req.body.content);
+    console.log(req.body.contents);
 
-    res.render('listAdd', { title: 'you submitted the form'});
+
+
+    const path = 'http://localhost:3000/test/users/'+req.params.id+'/notes';
+    var postdata = {
+        Username: 'new user',
+        Password: 'ne_pass',
+        Notes: {
+        title: req.body.title,
+        contents: req.body.contents
+    }
+    };
+    const requestOptions = {
+        url : path,
+        method : 'POST',
+        json : postdata
+
+
+    };
+
+    request(
+        requestOptions,
+        (function (err, response, body) {
+
+            console.log(body);
+
+            res.render('listAdd', { title: 'listAdd', notes: body});
+        })
+    );
 });
 
 module.exports = router;
--- routes/listEdit.js
+++ routes/listEdit.js
@@ -4,7 +4,42 @@ var router = express.Router();
 /* GET Editor List. */
 router.get('/', function(req, res, next) {
     res.render('listEdit', { title: 'listEdit'});
-    res.send('the list edit page');
 });
+
+
+/* Handle PUT from page form. */
+router.put('/', function(req, res, next) {
+
+    console.log('listEdit post from form');
+    console.log(req.body.title);
+    console.log(req.body.contents);
+    console.log(req.params.id);
+
+
+
+    const path = 'http://localhost:3000/test/users/'+req.params.id+'/notes/'+req.params.title;
+
+    var postdata = {
+        Notes: {
+            Title: req.body.title,
+            Contents: req.body.contents
+        }
+    };
+    const requestOptions = {
+        url : path,
+        method : 'PUT',
+        json : postdata };
+
+    request(
+        requestOptions,
+        (function (err, response, body) {
+
+            console.log(body);
+
+            res.render('listEdit', { title: 'listEdit', notes: body});
+        })
+    );
+});
+
 
 module.exports = router;
